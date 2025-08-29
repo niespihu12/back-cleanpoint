8223cb7e6c547558098d5a11828f90671233b898
unknown <niespihu12@gmail.com>
2025-08-28 19:40:43 -0500

    cambio en el cors

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app.include_router(compras.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://cleancoinm.netlify.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
